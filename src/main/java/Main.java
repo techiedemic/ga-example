d6422024a1345e2d4bfcc7c66a83e23f0d112546
Dwi Prasetyo <dwiprasetyo.an@gmail.com>
2020-02-29 20:56:38 +0100

    minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Stuff introducing new stuff");
+        System.out.println("Hellos, World! Stuff introducing new stuff");
     }
 }
